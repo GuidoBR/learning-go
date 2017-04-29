97de2722292c5c67dd4b330032461cc48118e2ca
Guido <guidopercu@gmail.com>
2017-04-29 14:40:29 -0300

    Termina passo 5

--- runas/runefinder.go
+++ runas/runefinder.go
@@ -10,26 +10,46 @@ import (
 	"log"
 )
 
-func AnalisarLinha(ucdLine string) (rune, string) {
+func AnalisarLinha(ucdLine string) (rune, string, []string) {
     campos := strings.Split(ucdLine, ";")
     código, _ := strconv.ParseInt(campos[0], 16, 32)
-    return rune(código), campos[1]
+	palavras := strings.Fields(campos[1])
+    return rune(código), campos[1], palavras
 }
 
 // Listar exibe na saída padrão o código, a runa e o nome dos caracteres Unicode
 // cujo nome contém o texto da consulta
 func Listar(texto io.Reader, consulta string) {
+	termos := strings.Fields(consulta)
     varredor := bufio.NewScanner(texto)
     for varredor.Scan() {
         linha := varredor.Text()
         if strings.TrimSpace(linha) == "" {
           continue
         }
-        runa, nome := AnalisarLinha(linha)
-        if strings.Contains(nome, consulta) {
+        runa, nome, palavrasNome := AnalisarLinha(linha)
+        if contémTodos(palavrasNome, termos) {
           fmt.Printf("U+%04X\t%[1]c\t%s\n", runa, nome)
         }
     }
+}
+
+func contém(fatia []string, procurado string) bool {
+	for _, item := range fatia {
+		if item == procurado {
+			return true
+		}
+	}
+	return false
+}
+
+func contémTodos(fatia []string, procurados []string) bool {
+	for _, procurado := range procurados {
+		if !contém(fatia, procurado) {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
--- runas/runefinder_test.go
+++ runas/runefinder_test.go
@@ -4,6 +4,7 @@ import (
         "testing"
         "strings"
 		"os"
+		"reflect"
 )
 
 const linhaLetraA = `0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;`
@@ -18,7 +19,7 @@ const linhas3Da43 = `
 `
 
 func TestAnalisarLinha(t *testing.T) {
-        runa, nome := AnalisarLinha(linhaLetraA)
+        runa, nome, palavras := AnalisarLinha(linhaLetraA)
         if runa != 'A' {
                 t.Errorf("Esperava 'A', veio %q", runa)
         }
@@ -26,6 +27,10 @@ func TestAnalisarLinha(t *testing.T) {
         if nome != nomeA {
             t.Errorf("Esperava %q, veio %q", nomeA, nome)
         }
+		palavrasA := []string{"LATIN", "CAPITAL", "LETTER", "A"}
+		if !reflect.DeepEqual(palavras, palavrasA) {
+			t.Errorf("\n\tEsperado: %q\n\trecebido: %q", palavrasA, palavras)
+		}
 }
 
 func ExampleListar() {
@@ -42,6 +47,26 @@ func ExampleListar_doisResultados() {
     // U+003E	>	GREATER-THAN SIGN
 }
 
+func ExampleListar_duasPalavras() {
+	texto := strings.NewReader(linhas3Da43)
+	Listar(texto, "CAPITAL LATIN")
+	// Output:
+	// U+0041	A	LATIN CAPITAL LETTER A
+	// U+0042	B	LATIN CAPITAL LETTER B
+	// U+0043	C	LATIN CAPITAL LETTER C
+}
+
+func Example_consultaDuasPalavras() {
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{"", "cat", "smiling"}
+	main()
+	// Output:
+	// U+1F638	😸	GRINNING CAT FACE WITH SMILING EYES
+	// U+1F63A	😺	SMILING CAT FACE WITH OPEN MOUTH
+	// U+1F63B	😻	SMILING CAT FACE WITH HEART-SHAPED EYES
+}
+
 func Example() {
         oldArgs := os.Args
         defer func() { os.Args = oldArgs }()
@@ -49,4 +74,47 @@ func Example() {
         main()
 		// Output:
 		// U+20A2	₢	CRUZEIRO SIGN
+}
+
+func TestContém(t *testing.T) {
+	var casos = []struct {
+		fatia		[]string
+		procurado	string
+		esperado	bool
+	} {
+		{[]string{"A", "B"}, "B", true},
+		{[]string{}, "A", false},
+		{[]string{"A", "B"}, "Z", false},
+	}
+	for _, caso := range casos {
+		obtido := contém(caso.fatia, caso.procurado)
+		if obtido != caso.esperado {
+			t.Errorf("contém(%#v, %#v) esperado: %v; recebido: %v",
+				caso.fatia, caso.procurado, caso.esperado, obtido)
+		}
+	}
+}
+
+func TestContémTodos(t *testing.T) {
+	casos := []struct {
+		fatia      []string
+		procurados []string
+		esperado   bool
+	}{
+		{[]string{"A", "B"}, []string{"B"}, true},
+		{[]string{}, []string{"A"}, false},
+		{[]string{"A"}, []string{}, true},
+		{[]string{}, []string{}, true},
+		{[]string{"A", "B"}, []string{"Z"}, false},
+		{[]string{"A", "B", "C"}, []string{"A", "C"}, true},
+		{[]string{"A", "B", "C"}, []string{"A", "Z"}, false},
+		{[]string{"A", "B"}, []string{"A", "B", "C"}, false},
+	}
+	for _, caso := range casos {
+		obtido := contémTodos(caso.fatia, caso.procurados)
+		if obtido != caso.esperado {
+			t.Errorf("contémTodos(%#v, %#v)\nesperado: %v; recebido: %v",
+				caso.fatia, caso.procurados, caso.esperado, obtido)
+		}
+	}
 }
